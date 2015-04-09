79520f46f82436c4cee9078299f8510dff0d3e50
Jonathan Cuesto <34cuesto@gmail.com>
2015-04-09 19:45:37 -0400

    second assignment done

--- ModuleTwoAssignment/Program.cs
+++ ModuleTwoAssignment/Program.cs
@@ -10,7 +10,46 @@ namespace ModuleTwoAssignment
     {
         static void Main(string[] args)
         {
-            Console.Write("something");
+            int mRow = 0;
+            char lastChar = 'O';
+            char x = 'X';
+            char o = 'O';
+            bool whileLoop = true;
+
+            while (whileLoop)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    if (lastChar == x)
+                    {
+                        Console.Write(o);
+                        lastChar = o;
+                    }
+                    else
+                    {
+                        Console.Write(x);
+                        lastChar = x;
+                    }
+                }
+                if (mRow < 7)
+                {
+                    if (lastChar == x)
+                    {
+                        lastChar = o;
+                    }
+                    else
+                    {
+                        lastChar = x;
+                    }
+                    mRow++;
+                    Console.WriteLine();
+                }
+                else
+                {
+                    whileLoop = false;
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
