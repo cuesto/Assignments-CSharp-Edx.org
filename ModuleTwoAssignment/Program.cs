322eb43c1302a0ea0360e235125fa0898cf5417f
Jonathan Cuesto <34cuesto@gmail.com>
2015-04-09 19:47:01 -0400

    second assignment

--- ModuleTwoAssignment/Program.cs
+++ ModuleTwoAssignment/Program.cs
@@ -48,7 +48,7 @@ namespace ModuleTwoAssignment
                 {
                     whileLoop = false;
                 }
-            }
+            } 
             Console.WriteLine();
         }
     }
