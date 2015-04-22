e62b56c5c9d8653d26912c8e63d94e98b477aa4f
Jonathan Cuesto <34cuesto@gmail.com>
2015-04-22 11:20:51 -0400

    assignment four finished

--- ModuleFourAssignment/Program.cs
+++ ModuleFourAssignment/Program.cs
@@ -10,6 +10,37 @@ namespace ModuleFourAssignment
     {
         static void Main(string[] args)
         {
+            Student[] students = new Student[5];
+
+            for(int i = 0 ; i < 5 ; i++)
+            {
+                students[i].name = "Jonathan";
+                Console.WriteLine(students[i].name);
+            }
+        }
+
+        public struct Student
+        {
+            public string name { get; set; }
+            public int age { get; set; }
+        }
+
+        public struct Teacher
+        {
+            public string name { get; set; }
+            public string speciality { get; set; }
+        }
+
+        public struct program
+        {
+            public string name { get; set; }
+            public string schedule { get; set; }
+        }
+
+        public struct Course
+        {
+            public string name { get; set; }
+            public string prerrequisite { get; set; }
         }
     }
 }
